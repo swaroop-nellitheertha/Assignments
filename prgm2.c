db82b0f77bce08f9c203c35a4d9048d98be5cebd
swaroop-nellitheertha <swaroopai1811@gmail.com>
2025-02-18 19:27:09 +0400

    prgm2.c

---
+++ prgm2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    
+    printf("Enter the number of rows for the triangle: ");
+    scanf("%d", &N);
+    
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
